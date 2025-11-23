7365c571dcb0ff378baa3d54b294ae16f049f5e9
PestrikovDD <theuselessfox1@gmail.com>
2025-11-23 17:26:32 +0500

    Task2

---
+++ Tyuiu.PestrikovDD.Sprint1.Task2.V17.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PestrikovDD.Sprint1.Task2.V17.Lib;
+
+namespace Tyuiu.PestrikovDD.Sprint1.Task2.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 60;
+            var res = ds.ConvertMinutesToHours(x);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.PestrikovDD.Sprint1.Task2.V17/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.PestrikovDD.Sprint1.Task2.V17.Lib;
+
+namespace Tyuiu.KokoulinIV.Sprint1.Task2.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил Пестриков Д. Д. | ИСТНБ-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Пестриков Данила Дмитриевич   | ИСТНБ-25-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на С#, которая запрашивает у пользователя данные ,   *");
+            Console.WriteLine("* вычисляет выражение колличество полных часов из минут                   *");
+            Console.WriteLine("* и печатает результат на экране                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  1ч = 60 мин                                                            *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите колличество минут");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.ConvertMinutesToHours(x));
+
+            Console.ReadKey();
+        }
+    }
+}
